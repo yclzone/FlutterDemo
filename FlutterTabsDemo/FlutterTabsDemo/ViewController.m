859b1ee5ed8a6865a352ab9196849063de231c79
Chenglin Yu <chenglin.yu@2wfyc.com>
2021-10-13 10:51:52 +0800

    using UIPageViewController

--- FlutterTabsDemo/FlutterTabsDemo/ViewController.m
+++ FlutterTabsDemo/FlutterTabsDemo/ViewController.m
@@ -13,7 +13,7 @@
 #import "FlutterAViewController.h"
 #import "FlutterBViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIPageViewControllerDataSource, UIPageViewControllerDelegate>
 @property (nonatomic, strong) NativeAViewController *nativeA;
 @property (nonatomic, strong) FBFlutterViewContainer *flutterA;
 @property (nonatomic, strong) FBFlutterViewContainer *flutterB;
@@ -23,9 +23,19 @@
 
 @implementation ViewController
 
+
+- (instancetype)initWithTransitionStyle:(UIPageViewControllerTransitionStyle)style navigationOrientation:(UIPageViewControllerNavigationOrientation)navigationOrientation options:(NSDictionary<UIPageViewControllerOptionsKey,id> *)options {
+    if (self = [super initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:options]) {
+        
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
     
     self.vcArray = @[].mutableCopy;
     
@@ -33,12 +43,21 @@
     self.flutterA = [self flutterWithPage:@"flutterA"];
     self.flutterB = [self flutterWithPage:@"flutterB"];
     
-    [self addVC:self.nativeA];
-    [self addVC:self.flutterA];
-    [self addVC:self.flutterB];
+//    [self addVC:self.nativeA];
+//    [self addVC:self.flutterA];
+//    [self addVC:self.flutterB];
     
-    self.segment.selectedSegmentIndex = 0;
-    [self.segment sendActionsForControlEvents:UIControlEventValueChanged];
+    [self.vcArray addObject:self.nativeA];
+    [self.vcArray addObject:self.flutterA];
+    [self.vcArray addObject:self.flutterB];
+    
+    
+    self.dataSource = self;
+    self.delegate = self;
+    [self setViewControllers:@[self.nativeA] direction:UIPageViewControllerNavigationDirectionReverse animated:YES completion:nil];
+    
+//    self.segment.selectedSegmentIndex = 0;
+//    [self.segment sendActionsForControlEvents:UIControlEventValueChanged];
 }
 
 - (FBFlutterViewContainer *)flutterWithPage:(NSString *)pageName {
@@ -59,10 +78,35 @@
 }
 
 - (IBAction)semgentOnChangd:(UISegmentedControl *)sender {
-    [self.vcArray enumerateObjectsUsingBlock:^(__kindof UIViewController * _Nonnull vc, NSUInteger idx, BOOL * _Nonnull stop) {
-        vc.view.hidden = (idx != sender.selectedSegmentIndex);
-    }];
+//    [self.vcArray enumerateObjectsUsingBlock:^(__kindof UIViewController * _Nonnull vc, NSUInteger idx, BOOL * _Nonnull stop) {
+//        vc.view.hidden = (idx != sender.selectedSegmentIndex);
+//    }];
     
+    [self setViewControllers:@[self.vcArray[sender.selectedSegmentIndex]] direction:UIPageViewControllerNavigationDirectionReverse animated:NO completion:nil];
+}
+
+#pragma mark - UIPageViewControllerDataSource
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
+    NSUInteger idx = [self.vcArray indexOfObject:viewController];
+    if (idx == 0 || idx == NSNotFound) {
+        return nil;
+    }
+    idx--;
+    return [self vcAtIndex:idx];
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
+    NSUInteger idx = [self.vcArray indexOfObject:viewController];
+    if (idx == 0 || idx == NSNotFound) {
+        return nil;
+    }
+    idx++;
+    return [self vcAtIndex:idx];
+}
+
+- (UIViewController *)vcAtIndex:(NSUInteger)idx {
+    return self.vcArray[idx];
 }
 
 @end
